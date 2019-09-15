765ac3b0949a55a5ae6e5c944e9d3fb71d314bb8
KuraiPiero <malmar1701@gmail.com>
2019-09-15 16:57:49 -0500

    creating api routes

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
-const express = require('express');
+/**echo "# PieroLetters" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/KuraiPiero/PieroLetters.git
+git push -u origin master
 
+
+…or push an existing repository from the command line
+
+git remote add origin https://github.com/KuraiPiero/PieroLetters.git
+git push -u origin master
+
+import
+…or import code from another repository
+You can initialize this repository with code from a Subversion, Mercurial, or TFS project.**/
+
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv/config')
+
+//*Import Routes
+const stockRoute = require('./routes/stock');
+app.use('/api', stockRoute);
+
+//*Middlewares
+
 
 //!Routes
 
-app.get('/api', (req, res) => {
-    res.send('Entering PieroLetters')
+app.get('/', (req, res) => {
+    res.send('Entering Piero Letters')
 });
+
+//TODO Mongo Connection
+mongoose.connect(
+    process.env.DB_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    () => console.log('Connected to Data Base')
+);
+
 
 //?Server Listener
 app.listen(9000)
